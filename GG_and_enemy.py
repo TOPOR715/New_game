1bdb63d8e8c821492e37cda2926a3248f297bd99
Turist <Foksiigoldi@gmail.com>
2026-06-10 00:00:13 +0900

    ping

--- GG_and_enemy.py
+++ GG_and_enemy.py
@@ -1,5 +1,48 @@
 class Player():
     def glavniy_geroy():
-        print("dsadasd")
+        head, thorax, stomach = 35, 85, 70
+        left_arm, right_arm = 60, 60
+        left_leg, right_leg = 65, 65
+        
+        data = {
+            "Name": None,
+            "Firstname": None,
+            "Age": None,
+            "dop_data": None,
+            "inventar": [],
+            "head": head,
+            "thorax": thorax,
+            "stomach": stomach,
+            "left arm": left_arm,
+            "right arm": right_arm,
+            "left leg": left_leg,
+            "right leg": right_leg,
+            "full_hp": head + thorax + stomach + left_arm + right_arm + left_leg + right_leg
+        }
+        return data
 
-Player.glavniy_geroy()
+
+class Enemy(): 
+    def bandit():
+        head, thorax, stomach = 35, 85, 70
+        left_arm, right_arm = 60, 60
+        left_leg, right_leg = 65, 65
+        
+        data = {
+            "Name": None,
+            "Firstname": None,
+            "Age": None,
+            "dop_data": None,
+            "inventar": [],
+            "head": head,
+            "thorax": thorax,
+            "stomach": stomach,
+            "left arm": left_arm,
+            "right arm": right_arm,
+            "left leg": left_leg,
+            "right leg": right_leg,
+            "full_hp": head + thorax + stomach + left_arm + right_arm + left_leg + right_leg
+        }
+        return data
+
+# sadsadasd
